cbe19af3fb2878924f6159e70b5101d0cacf633c
Johnatas michaell <johnatasmichael067@gmail.com>
2025-07-14 16:58:38 -0300

    add_javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelected = document.getElementById('gameSelected')
+const questionInput = document.getElementById('questionInput')
+const askButton =  document.getElementById('askButton')
+const form = document.getElementById('form')
+
